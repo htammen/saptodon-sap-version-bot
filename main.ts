c4c233615e47683a47ce0ad48295f6f1990fa418
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2024-05-08 14:00:57 +0200

    feat: add url if maven is the provider

--- main.ts
+++ main.ts
@@ -8,6 +8,7 @@ function parseRequest(json: object): string {
   let url = ""
   if (json.provider === "github") { url = `https://github.com/${json.project}` }
   if (json.provider === "npm") { url = `https://www.npmjs.com/package/${json.project}` }
+  if (json.provider === "maven") { url = `https://central.sonatype.com/artifact/${json.project.replace(':', '/')}` }
   return `There is a new version of\n${json.project}\non ${json.provider}\nversion: ${json.version}${url ? `\n${url}` : ``}`
 }
 
